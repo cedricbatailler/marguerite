5351f0c543dd8e3b28408691ba4df02cfb6fc6f3
Cedric Batailler <cedric.batailler@gmail.com>
2020-09-04 13:45:25 +0200

    Add tests for decode_firebase_timestamp

---
+++ tests/testthat/test-decode_firebase_timestamp.R
@@ -0,0 +1,15 @@
+test_that("decode_firebase_timestamp produced timestamps as intended", {
+
+    firebase_id           <- "-MFdkH5qPe_6NCTqZ_3y"
+    firebase_id_truncated <- "-MFdkH5q"
+
+    firebase_timestamp    <- 1598428357046 / 1000
+
+    firebase_id_decoded <- decode_firebase_timestamp(firebase_id)
+    firebase_id_truncated_decoded <- decode_firebase_timestamp(firebase_id_truncated)
+
+    expect_equal(firebase_id_decoded, firebase_timestamp)
+    expect_equal(firebase_id_truncated_decoded, firebase_timestamp)
+    expect_equal(firebase_id_decoded, firebase_id_truncated_decoded)
+
+})
